a43bf730e0016316e8461322bc28e1ceaf23c3f8
minjung / 정민정 <minjungx2@gmail.com>
2022-06-13 21:05:36 +0900

    입력값  localStorage에 저장

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,18 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    text-align: center;
+    background-color: ghostwhite;
+}
+input {
+    border-style: groove;
+    width: 200px;
+}
+button {
+    border-style: groove;
+}
+.shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,64 @@
 <template>
-    <div>input</div>
+    <div class="inputBox shadow">
+        <input 
+        type="text" 
+        v-model="newTodoItem"
+        placeholder="할 일을 입력해주세요"
+        v-on:keyup.enter="addTodo"
+        >
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+        </span>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
 
+    methods: {
+        addTodo() {
+            if (this.newTodoItem !== "") {
+                let value = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }  
+        },
+
+        clearInput() {
+            this.newTodoItem = "";
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 epx 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
